c198b811ff994e18f53d0811901fa56560265c2c
Yeimy Rodríguez <ydrodrigueza10a@gmail.com>
2025-10-22 14:17:04 -0500

    Agregación de una modificación desde la rama feature

--- src/App.java
+++ src/App.java
@@ -2,5 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("HOLA!");
         System.out.println("¿Cómo estás?");
+
+        System.out.println("Modificacion desde una rama deature");
+
+        System.out.println("Otra modificacion desde la rama feature");
     }
 }
